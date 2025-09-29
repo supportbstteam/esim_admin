b2ed8e204da241573c8a9f1c12dff2a9ca82e897
bstteam102 <bstteam102@gmail.com>
2025-09-29 17:01:37 +0530

    added css and to make component look similar

--- src/app/admin/country/page.tsx
+++ src/app/admin/country/page.tsx
@@ -80,11 +80,11 @@ function Country() {
             setEditCountry(null);
             setIsAddModalOpen(true);
           }}
-          className="flex items-center gap-2 px-6 py-3 font-bold rounded-lg bg-green-600 text-white hover:bg-green-700 transition"
+          className="bg-[#16325d] text-white rounded px-4 py-2 mb-4"
         >
-          <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          {/* <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
-          </svg>
+          </svg> */}
           Add Country
         </button>
       </div>
