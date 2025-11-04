33d1e6f94c98c603ad2d95e06e62979741486747
bstteam102 <bstteam102@gmail.com>
2025-11-04 13:01:27 +0530

    refactor: on loading data shown

--- src/components/Cards/CustomerInfo.tsx
+++ src/components/Cards/CustomerInfo.tsx
@@ -6,7 +6,7 @@ function CustomerInfo({ name, email, phone }) {
         <div className="shadow border rounded py-5 px-5" >
             <h3 className="text-xl font-semibold text-black " >Customer Details</h3>
             <div className="flex items-center justify-between mt-5"  >
-                <span className="text-black flex-1 flex items-center gap-1" > <CiUser size={25} />{name}</span>
+               {name && <span className="text-black flex-1 flex items-center gap-1" > <CiUser size={25} />{name}</span>}
                 {email && <span className="text-black flex-1 flex items-center gap-1 " > <CiMail size={25} /> {email}</span>}
                 {phone && <span className="text-black flex-1 flex items-center gap-1 " > <CiPhone size={25} /> {phone}</span>}
 
